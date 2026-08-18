732069ac04b1d8bcdf601b731a3032f3f4c99a5d
bhoomika1313-ally <bhoomikaa1313@gmail.com>
2026-08-18 10:11:21 +0530

    add app.js v1

--- lab3/app.js
+++ lab3/app.js
@@ -3,6 +3,7 @@ const server = http.createServer();
 server.on('request', (req, res) => {
     res.write("<h1> welcome to server side programming, yay </h1>");
     res.write("<h2> nodemon is running </h2>");
+    res.write("<h3> this is a simple server </h3>");
     res.end();
 });
 
